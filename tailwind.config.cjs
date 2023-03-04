bffe314272c847f0e8783be643c06f7d348ea71d
Никита Дурнев <nikitadurnev@Nikitas-MacBook-Air.local>
2023-03-04 22:25:58 +0200

    feat: add form markup, styles

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      primary: "#0d0d0d",
+      secondary: "#D3D3D2",
+      success: "#75B240",
+      fail: "#DA7225",
+      footerBg: "#F7F6F5",
+      buttonBg: "#FDFDFD",
+    },
+    screens: {
+      xs: "400px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      transitionDuration: { 200: "200ms" },
+      transitionProperty: {},
+    },
+  },
+  variants: {
+    extend: { texColor: ["responsive", "hover", "focus", "focus-within"] },
+  },
+  plugins: [
+    plugin(function ({ addBase, pointer }) {
+      addBase({
+        button: { cursoir: pointer },
+      });
+    }),
+  ],
+};
